f409f0b042fb2599867e7f03adefc8c513cc71e7
Sowwtky <miche46ale@gmail.com>
2022-06-17 14:31:39 +0200

    renamed classes

--- src/jana60/Biglietteria.java
+++ src/jana60/Biglietteria.java
@@ -2,4 +2,9 @@ package jana60;
 
 public class Biglietteria {
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
 }
--- src/jana60/Biglietto.java
+++ src/jana60/Biglietto.java
@@ -2,4 +2,21 @@ package jana60;
 
 public class Biglietto {
 
+	private double chilometri;
+	private int anniPasseggero;
+	
+	public Biglietto(double chilometri, int anniPasseggero) {
+		this.anniPasseggero = anniPasseggero;
+		this.chilometri = chilometri;
+	}
+
+	public void setChilometri(double chilometri) {
+		this.chilometri = chilometri;
+	}
+
+	public void setAnniPasseggero(int anniPasseggero) {
+		this.anniPasseggero = anniPasseggero;
+	}
+	
+	
 }
